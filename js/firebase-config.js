2ed67db54cf35361fc4c9b68e9ac1a0cf92c96e1
psyclopse <olamionabanjo0.1@gmail.com>
2025-08-02 01:49:51 +0100

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,8 +1,11 @@
-// Firebase configuration and initialization
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js';
-import { getFirestore, connectFirestoreEmulator } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Firebase configuration
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCPO1dHYC7vPKc3n0Es4dW96u-5v85Ijx8",
   authDomain: "name-it-2.firebaseapp.com",
@@ -13,18 +16,6 @@ const firebaseConfig = {
   measurementId: "G-LBJWRHQNKC"
 };
 
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-// For development, you can use the Firestore emulator
-// Uncomment the following lines if you want to use the emulator
-// if (location.hostname === 'localhost') {
-//     connectFirestoreEmulator(db, 'localhost', 8080);
-// }
-
-// Export the database instance
-window.db = db;
-
-export { db };
+const analytics = getAnalytics(app);
